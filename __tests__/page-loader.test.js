ed5cb187cbec9202f3db448d5465716e9ca163bf
roman_ushakov <roman.ushakov.89@inbox.ru>
2019-09-23 21:17:06 +0300

    laod assets

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -4,16 +4,29 @@ import path from 'path';
 import nock from 'nock';
 import loadPage from '../src';
 
-test('page-loader', async () => {
-  const tmpdir = os.tmpdir();
-  const expected = await fs.readFile(path.join(__dirname, '__fixtures__/index.html'), 'utf8');
+const getFixrurePath = (fileName) => path.join(__dirname, '__fixtures__', fileName);
 
-  nock('http://hexlet.io')
-    .get('/courses')
-    .reply(200, expected);
+const host = 'http://hexlet.io';
+
+let tmpdir;
+
+beforeEach(async () => {
+  tmpdir = await fs.mkdtemp(os.tmpdir());
+});
+
+test('page-loader', async () => {
+  nock(host).get('/courses').replyWithFile(200, getFixrurePath('index.html'));
+  nock(host).get('/assets/inferno.jpg').replyWithFile(200, getFixrurePath('assets/inferno.jpg'));
+  nock(host).get('/assets/styles.css').replyWithFile(200, getFixrurePath('assets/styles.css'));
 
   await loadPage('http://hexlet.io/courses', tmpdir);
 
-  const result = await fs.readFile(path.join(tmpdir, 'hexlet-io-courses.html'), 'utf8');
+  const [expected, result, assets] = await Promise.all([
+    fs.readFile(getFixrurePath('result.html'), 'utf8'),
+    fs.readFile(path.join(tmpdir, 'hexlet-io-courses.html'), 'utf8'),
+    fs.readdir(path.join(tmpdir, 'hexlet-io-courses_files')),
+  ]);
+
   expect(result).toMatch(expected);
+  expect(assets).toEqual(['inferno.jpg', 'styles.css']);
 });
--- src/index.js
+++ src/index.js
@@ -1,14 +1,70 @@
 import { promises as fs } from 'fs';
 import path from 'path';
+import url from 'url';
+import { startsWith, isEmpty } from 'lodash';
 import { get } from 'axios';
+import cheerio from 'cheerio';
+
+const makeDirectoryName = (target) => target.replace(/^https?:\/\//, '').replace(/\W/g, '-');
+
+const tagsSrcNames = {
+  IMG: 'src',
+  SCRIPT: 'src',
+  LINK: 'href',
+};
+
+const isLocalSource = (el) => {
+  const srcName = tagsSrcNames[el.prop('tagName')];
+  const src = el.attr(srcName);
+
+  if (!src) {
+    return false;
+  }
+
+  return !startsWith(src, 'http');
+};
+
+const replaceAssets = (html, assetPath) => {
+  const $ = cheerio.load(html);
+
+  const oldPaths = $('img, link, script')
+    .filter((_, el) => isLocalSource($(el)))
+    .map((_, el) => {
+      const srcName = tagsSrcNames[$(el).prop('tagName')];
+
+      const oldAttr = $(el).attr(srcName);
+      const newAttr = path.join(assetPath, path.basename(oldAttr));
+
+      $(el).attr(srcName, newAttr);
+
+      return oldAttr;
+    }).toArray();
+
+  return { html: $.html(), oldPaths };
+};
 
 export default (target, output) => get(target)
   .then(({ data }) => {
-    const fileName = target
-      .replace(/^https?:\/\//, '')
-      .replace(/\W/g, '-')
-      .concat('.html');
+    const targetUrl = url.parse(target);
+    const dirName = makeDirectoryName(target);
+    const assetsDir = `/${dirName}_files`;
 
-    const filePath = path.join(output, fileName);
-    return fs.writeFile(filePath, data);
-  });
+    const { html, oldPaths } = replaceAssets(data, assetsDir);
+
+    return fs.writeFile(path.join(output, `${dirName}.html`), html)
+      .then(() => {
+        if (isEmpty(oldPaths)) {
+          return true;
+        }
+
+        const assetsPath = path.join(output, assetsDir);
+        return fs.mkdir(assetsPath)
+          .then(() => Promise.all(oldPaths.map((pathname) => {
+            const assetUrl = url.format({ ...targetUrl, pathname });
+            return get(assetUrl).then((res) => {
+              const assetPath = path.join(assetsPath, 'TTT', path.basename(pathname));
+              return fs.writeFile(assetPath, res.data);
+            });
+          })));
+      });
+  }).then();
