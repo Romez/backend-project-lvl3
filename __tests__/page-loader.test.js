be46aa09f9ccd8ec9ee7975fb366307521679ae9
roman_ushakov <roman.ushakov.89@inbox.ru>
2019-10-18 21:08:27 +0300

    feat: add step 2

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -2,7 +2,7 @@ import { promises as fs } from 'fs';
 import os from 'os';
 import path from 'path';
 import nock from 'nock';
-import loadPage, { makeFileName } from '../src';
+import loadPage from '../src';
 
 const getFixturesPath = (fileName) => path.join(__dirname, '__fixtures__', fileName);
 
@@ -13,24 +13,24 @@ beforeEach(async () => {
   tmpdir = await fs.mkdtemp(path.join(os.tmpdir(), '/'));
 });
 
-const targetsSet = [
-  ['http://hexlet.io/courses', 'hexlet-io-courses.html'],
-  ['http://hexlet.io/courses/', 'hexlet-io-courses.html'],
-];
-
-test.each(targetsSet)('should_make_valid_name from "%s" to "%s"', (target, result) => {
-  expect(makeFileName(target)).toBe(result);
-});
-
-test('should_save_page', async () => {
-  nock(host).get('/courses').replyWithFile(200, getFixturesPath('index.html'));
+test('should_save_page_and_assets', async () => {
+  nock(host).get('/courses')
+    .replyWithFile(200, getFixturesPath('index.html'))
+    .get('/assets/img.jpg')
+    .replyWithFile(200, getFixturesPath('img.jpeg'))
+    .get('/assets/index.js')
+    .replyWithFile(200, getFixturesPath('index.js'))
+    .get('/assets/styles.css')
+    .replyWithFile(200, getFixturesPath('styles.css'));
 
   await loadPage(`${host}/courses`, tmpdir);
 
-  const [expected, result] = await Promise.all([
-    fs.readFile(getFixturesPath('index.html'), 'utf8'),
+  const [expected, result, assets] = await Promise.all([
+    fs.readFile(getFixturesPath('result.html'), 'utf8'),
     fs.readFile(path.join(tmpdir, 'hexlet-io-courses.html'), 'utf8'),
+    fs.readdir(path.join(tmpdir, 'hexlet-io-courses_files')),
   ]);
 
   expect(result).toMatch(expected);
+  expect(assets).toEqual(['assets-img.jpg', 'assets-index.js', 'assets-styles.css']);
 });
--- src/index.js
+++ src/index.js
@@ -2,21 +2,81 @@ import { promises as fs } from 'fs';
 import path from 'path';
 import url from 'url';
 import axios from 'axios';
-import _ from 'lodash';
+import { trim, flow } from 'lodash';
+import cheerio from 'cheerio';
 
-export const makeFileName = _.flow(
+export const makeFileName = flow(
   url.parse,
   ({ hostname, pathname }) => `${hostname}${pathname}`,
-  (value) => _.trim(value, '/'),
+  (value) => trim(value, '/'),
   (value) => value.replace(/\W/g, '-'),
-  (value) => `${value}.html`,
 );
 
+const makeAssetName = (target) => {
+  const ext = path.extname(target);
+
+  return trim(target, '/')
+    .replace(ext, '')
+    .replace(/\W/g, '-')
+    .concat(ext);
+};
+
+const isLocal = /^((?!data|http).*)/gi;
+
+const attrNames = { link: 'href', img: 'src', script: 'src' };
+
+const replaceAssets = (html, baseName) => {
+  const $ = cheerio.load(html);
+
+  const replacedPaths = [];
+
+  $('img, link, script')
+    .filter((_, el) => {
+      const val = $(el).prop(attrNames[el.tagName]);
+      // console.log(val)
+      return isLocal.test(val);
+    })
+    .each((id, el) => {
+      const attrName = attrNames[el.tagName];
+      const oldVal = $(el).prop(attrName);
+
+      const newVal = path.join(`${baseName}_files`, makeAssetName(oldVal));
+      // console.log(newVal)
+      $(el).attr(attrName, newVal);
+
+      replacedPaths[id] = oldVal;
+    });
+
+  return [$.html(), replacedPaths];
+};
+
 export default (target, output) => {
+  const baseName = makeFileName(target);
+  const assetsPath = path.join(output, `${baseName}_files`);
+  let pathnames = [];
+
   return axios.get(target)
     .then((response) => {
-      const filepath = path.resolve(output, makeFileName(target));
-      // console.log(filepath);
-      return fs.writeFile(filepath, response.data);
+      const filepath = path.resolve(output, `${baseName}.html`);
+
+      const [html, replacedPaths] = replaceAssets(response.data, baseName);
+      pathnames = replacedPaths;
+
+      return fs.writeFile(filepath, html);
+    })
+    .then(() => fs.mkdir(assetsPath))
+    .then(() => {
+      const urls = pathnames.map((pathname) => url.resolve(target, pathname));
+      const promises = urls.map((assetUrl) => axios.get(assetUrl));
+      return Promise.all(promises);
+    })
+    .then((responses) => {
+      const promises = responses.map((response) => {
+        const filepath = path.join(assetsPath, makeAssetName(response.request.options.pathname));
+        // console.log(filepath);
+        return fs.writeFile(filepath, response.data);
+      });
+
+      return Promise.all(promises);
     });
 };
